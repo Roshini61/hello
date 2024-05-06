a544d5ea12df6db39c8e87b815168025e4b6f969
Roshini61 <roshinireddy2893@gmail.com>
2024-05-06 14:15:21 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,26 @@ def firstRepeatingElement(arr, n):
        
   # If no repeating element is found, return -1
   return -1
- 
+def lastRepeatingElement(arr, n):
+    # Initialize a dictionary to store the last index of each element
+    last_index = {}
+    # Initialize a variable to store the index of the last repeating element found so far
+    last_repeating_index = -1
+
+    # Traverse the array from right to left
+    for i in range(n - 1, -1, -1):
+        # If the element is encountered for the first time, store its index
+        if arr[i] not in last_index:
+            last_index[arr[i]] = i
+        else:
+            # If the element is repeating, update the last repeating index
+            last_repeating_index = max(last_repeating_index, last_index[arr[i]])
+
+    # If no repeating element is found, return -1
+    if last_repeating_index == -1:
+        return -1
+    else:
+        return last_repeating_index
 # Driver code
 if __name__ == '__main__':
   # Initializing an array and its size
@@ -24,3 +43,4 @@ if __name__ == '__main__':
   else:
       # Printing the first repeating element and its index
       print("First repeating element is", arr[index])
+    
